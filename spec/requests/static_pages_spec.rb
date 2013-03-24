d8180bcb2bea5c92df5be21795a770f2e58ac37a
Xavier CROUILLES <xavier.crouilles@gmail.com>
2013-03-24 18:39:04 +0100

    Finish layout and routes

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,42 +2,41 @@ require 'spec_helper'
 
 describe "Static pages" do
 
+  subject { page }
+
   describe "Home page" do
 
-    it "should have the title 'Home'" do
-      visit '/static_pages/home'
-      expect(page).to have_title("Ruby on Rails Tutorial Sample App | Home")
-    end
+  	before { visit root_path }
 
-    it "should have the h1 'Sample App'" do
-      visit '/static_pages/home'
-      page.should have_selector('h1', :text => 'Sample App')
-    end
+    it { should have_title(full_title('Home')) }
+    it { should have_selector('h1', :text => 'Sample App') }
+    
   end
 
   describe "Help page" do
 
-    it "should have the h1 'Help'" do
-      visit '/static_pages/help'
-      page.should have_selector('h1', :text => 'Help')
-    end
+  	before { visit help_path }
 
-    it "should have the title 'Help'" do
-      visit '/static_pages/help'
-      expect(page).to have_title("Ruby on Rails Tutorial Sample App | Help")
-    end
+    it { should have_title(full_title('Help')) }
+    it { should have_selector('h1', :text => 'Help') }
+
   end
 
   describe "About page" do
 
-    it "should have the h1 'About Us'" do
-      visit '/static_pages/about'
-      page.should have_selector('h1', :text => 'About Us')
-    end
+  	before { visit about_path }
 
-    it "should have the title 'About Us'" do
-      visit '/static_pages/about'
-      expect(page).to have_title("Ruby on Rails Tutorial Sample App | About Us")
-    end
+    it { should have_title(full_title('About Us')) }
+    it { should have_selector('h1', :text => 'About Us') }
+
+  end
+
+  describe "Contact page" do
+
+  	before { visit contact_path }
+
+    it { should have_title(full_title('Contact')) }
+    it { should have_selector('h1', :text => 'Contact') }
+
   end
 end
